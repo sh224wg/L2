540f10bde95a9128809b30a44245a3667d40c95e
Saskia H <sh224wg@student.lnu.se>
2024-09-27 19:03:48 +0200

    okay wrote function to ask user for url and write to file using fs beacuse it is too large and formatting will be easier

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
 import WebScraper from './src/scraper.js'
+import readline from 'readline'
+import fs from 'fs'
 
 const webScraper = new WebScraper()
 
+function askForUrl() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+    rl.question('Please Enter URL to scrape: ', (url) => {
+        rl.close()
+        run(url)
+    })
+}
+
 // seperate into two functions one that runs and another that asks user for url?
-async function run() {
-    const url = 'https://www.svt.se/nyheter/utrikes/experten-darfor-ar-konflikten-mellan-israel-och-hizbollah-att-klassa-som-ett-krig'
+async function run(url) {
+    //const url = 'https://www.svt.se/nyheter/utrikes/experten-darfor-ar-konflikten-mellan-israel-och-hizbollah-att-klassa-som-ett-krig'
     try {
         const content = await webScraper.scrape(url)
+        const format = {
+            titles: content.titles || [],
+            paragraphs: content.paragraphs || [],
+            images: content.images || [],
+            links: content.links || [],
+            spans: content.spans || [],
+            lists: content.lists || []
+        }
+
+        const scrapedData = JSON.stringify(format, null, 2)
+        fs.writeFileSync('scrapedContent.json', scrapedData)
+        console.log('Scraped data can be found in scrapedContent.json')
         //elements.forEach((div, index) => {
           //  console.log(`Div${index + 1}:`)
-            console.log('Titles:', content.titles)
+         /*    console.log('Titles:', content.titles)
             console.log('Paragraphs:', content.paragraphs)
             console.log('Images:', content.images)
             console.log('Links:', content.links)
             console.log('Spans:', content.spans)
-            console.log('Lists:', content.lists)
+            console.log('Lists:', content.lists) */
 
     } catch (error) {
         console.error(error)
     }
 }
-run()
+
+askForUrl()
